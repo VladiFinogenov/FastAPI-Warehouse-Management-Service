0e55dafb6333be6ae948fd6d52bd2dedd6e98e95
vlad <Finogenov.V.A@yandex.ru>
2024-09-25 16:09:57 +0300

    fix: перенесена часть логики в сервис

--- app/api/routes/orders.py
+++ app/api/routes/orders.py
@@ -1,14 +1,14 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.data.models.order import Order
-from app.domain.schemas.schemas import OrderCreate, OrderResponse
+from app.domain.schemes.order_scheme import OrderCreate, OrderDetail, OrderState
 from app.domain.services.order_service import OrderService
 from app.core.backend.db_depends import get_db_async
 from app.data.repositories.order_repository import OrderRepository
 from app.data.repositories.product_repository import ProductRepository
+
 
 router = APIRouter(tags=['orders'])
 
@@ -19,42 +19,32 @@ def get_order_service(db: AsyncSession = Depends(get_db_async)) -> OrderService:
     return OrderService(order_repository, product_repository)
 
 
-@router.post("/orders", response_model=OrderResponse)
+@router.post("/orders")
 async def create_order(
         order_data: OrderCreate,
-        service: Annotated[OrderService, Depends(get_order_service)]
-):
-    order = Order(**order_data.model_dump())
-    return await service.create_order(order)
+        service: Annotated[OrderService, Depends(get_order_service)]):
 
-@router.get("/orders", response_model=list[OrderResponse])
-async def get_all_orders(service: OrderService = Depends(get_order_service)):
-    return await service.get_all_orders()
+    return await service.create(order_data)
 
-@router.get("/orders/{id}", response_model=OrderResponse)
-async def get_order(order_id: int, service: OrderService = Depends(get_order_service)):
-    order = await service.get_order_by_id(order_id)
-    if order is None:
-        raise HTTPException(status_code=404, detail="Order not found")
-    return order
 
-@router.put("/orders/{id}", response_model=OrderResponse)
-async def update_order(order_id: int, order_data: OrderCreate, service: OrderService = Depends(get_order_service)):
-    order = await service.get_order_by_id(order_id)
-    if order is None:
-        raise HTTPException(status_code=404, detail="Order not found")
+@router.get("/orders")
+async def get_all_orders(service: Annotated[OrderService, Depends(get_order_service)]):
+    return await service.get_all()
 
-    for key, value in order_data.model_dump().items():
-        setattr(order, key, value)
 
-    await service.update_order(order)
-    return order
+@router.get("/orders/{id}")
+async def get_order(
+        order_id: int,
+        service: Annotated[OrderService, Depends(get_order_service)]):
 
-@router.delete("/orders/{id}")
-async def delete_order(order_id: int, service: OrderService = Depends(get_order_service)):
-    order = await service.get_order_by_id(order_id)
-    if order is None:
-        raise HTTPException(status_code=404, detail="Order not found")
+    return await service.get_by_id(order_id)
 
-    await service.delete_order(order_id)
-    return {"detail": "Order deleted"}
+
+@router.put("/orders/{id}")
+async def update_order_status(
+        order_id: int,
+        order_data: OrderState,
+        service: Annotated[OrderService, Depends(get_order_service)]):
+
+    await service.update_status(order_id, order_data.status)
+    return await service.get_by_id(order_id)

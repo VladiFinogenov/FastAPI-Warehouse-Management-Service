eb2e07166a536f34dfa0b81e260768895ef6b45b
vlad <Finogenov.V.A@yandex.ru>
2024-09-25 16:12:34 +0300

    feat: добавлена логика обработки запросов

--- app/data/repositories/order_repository.py
+++ app/data/repositories/order_repository.py
@@ -1,24 +1,28 @@
-from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
-from app.data.models.order import Order
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
+
+from app.data.models import Order
 
 
 class OrderRepository:
     def __init__(self, db: AsyncSession):
         self.db = db
 
-    async def create(self, order: Order) -> Order:
+    async def create(self, order: Order) -> None:
         self.db.add(order)
         await self.db.commit()
         await self.db.refresh(order)
-        return order
 
     async def get_all(self):
         result = await self.db.execute(select(Order))
         return result.scalars().all()
 
     async def get_by_id(self, order_id: int):
-        result = await self.db.execute(select(Order).filter(Order.id == order_id))
+        result = await self.db.execute(
+            select(Order)
+            .options(selectinload(Order.items))
+            .filter(Order.id == order_id))
         return result.scalars().first()
 
     async def update(self, order: Order):

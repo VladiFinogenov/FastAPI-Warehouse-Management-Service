fccb015a9b4de302e46bab5f4fa135805934fb97
vlad <Finogenov.V.A@yandex.ru>
2024-09-27 10:08:14 +0300

    fix: удален лишний импорт

--- app/data/models/order.py
+++ app/data/models/order.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, event
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 
 from app.core.backend.db import Base
